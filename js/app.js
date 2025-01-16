821e0cf2dfad72a270b6c91df460ba3e5d17db69
juancarlosfc5 <181221774+juancarlosfc5@users.noreply.github.com>
2025-01-16 12:34:23 -0500

    feat: :sparkles: v3 - Promesas
    Se incluyen las promesas dentro de las funciones

--- js/app.js
+++ js/app.js
@@ -5,10 +5,16 @@ import { search } from "./methods.js";
 const result = document.getElementById("results"); //Definir contenedor de salida
 
 // Agrega un evento de click al botón
-//document.getElementById('button').addEventListener('click', search(movies, quest, result));
-
-document.getElementById('button').addEventListener('click',(e) =>{
-    const quest = document.getElementById("input").value.toLowerCase(); 
-    result.innerHTML = ""; // Limpiar resultados anteriores
-    search(movies, quest, result)
-})
+document.getElementById("button").addEventListener("click", (e) => {
+  const quest = document.getElementById("input").value.toLowerCase();
+  result.innerHTML = ""; // Limpiar resultados anteriores
+  search(movies, quest, result)
+    .then((resolveFunction) => {
+      // Ejecuta la función pasada en el resolve
+      resolveFunction();
+    })
+    .catch((rejectFunction) => {
+      // Ejecuta la función pasada en el reject
+      rejectFunction();
+    });
+});
--- js/methods.js
+++ js/methods.js
@@ -1,52 +1,33 @@
 // Función de búsqueda
 export function search(movies, quest, result) {
-  
-  if (quest) {
-    // Filtrar los libros que contienen la palabra en la descripción
-    const filter = movies.filter((movie) =>
-      movie.title.toLowerCase().includes(quest)
-    );
+  return new Promise((resolve, reject) => {
+    if (!quest) {
+      alert("No se ingresaron datos para realizar la búsqueda."); //Mensaje de alerta de validación para no busqueda
+    } else {
+      // Filtrar los libros que contienen la palabra en la descripción
+      const filter = movies.filter((movie) =>
+        movie.title.toLowerCase().includes(quest)
+      );
+      console.log(filter);
 
-    if (filter.length > 0) {
-      filter.forEach((movie) => {
-        const {title, poster, summary} = movie
-        const div = document.createElement("div"); //Crear el div que contendra la tarjeta
-        div.innerHTML = `
+      if (filter.length > 0) {
+        resolve(() => 
+        filter.forEach((movie) => {
+          const { title, poster, summary } = movie;
+          const div = document.createElement("div"); //Crear el div que contendra la tarjeta
+          div.innerHTML = `
             <h3>${title}</h3>
             <img src="${poster}" alt="">
             <p>${summary}</p>
             <button data-name="${title}" >Ver detalle</button>
           `; //Incluir tarjeta en el div
-        result.appendChild(div); //Agregar tarjeta al div "results"
-      });
-    } else {
-      result.innerHTML = "<br> No se encontraron resultados para tu busqueda."; //Mensaje de validación para no coincidencias
+          result.appendChild(div); //Agregar tarjeta al div "results"
+        })
+      )} else {
+        reject(() => {
+          result.innerHTML = "<br> No se encontraron resultados para su busqueda."
+        });
+      }
     }
-  }
+  });
 }
-
-// Sintaxis Promesa
-// const getHeroeByIdAsync = (id) => {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       const p1 = getHeroeById(id);
-//       if (p1) {
-//         resolve(p1); // Resuelve con el héroe encontrado
-//       } else {
-//         reject('No se pudo encontrar el héroe'); // Rechaza si no se encuentra
-//       }
-//     }, 2000);
-//   });
-// };
-
-// // Uso de la función asíncrona
-// getHeroeByIdAsync(2)
-//   .then((heroe) => {
-//     console.log('Héroe encontrado:', heroe); // { id: 2, name: 'Superman' }
-//   })
-//   .catch((error) => {
-//     console.warn(error); // "No se pudo encontrar el héroe" si falla
-//   });
-
-//export function detail(){}
-
